cde15ae89f2003dcaaa754be126a15e6d6035e6e
naosuke <info@naosuke.me>
2019-01-05 14:30:31 +0900

    :pencil: practice "range continued"

--- range.go
+++ range.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	pow := []int{1, 2, 4, 8, 16, 32, 64, 128}
+	pow := make([]int, 10)
+	for i := range pow {
+		pow[i] = 1 << uint(i)
+	}
 
-	for i, v := range pow {
-		fmt.Printf("2**%d = %d\n", i, v)
+	for _, value := range pow {
+		fmt.Printf("%d\n", value)
 	}
 }

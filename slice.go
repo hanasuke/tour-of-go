97ec7f97e621e360e7813be69337248a8049d620
naosuke <info@naosuke.me>
2019-01-05 13:49:33 +0900

    :pencil: practice "appending to slice"

--- slice.go
+++ slice.go
@@ -2,22 +2,22 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
-	board := [][]string {
-		{"-", "-", "-"},
-		{"-", "-", "-"},
-		{"-", "-", "-"},
-	}
-	board[0][0] = "X"
-	board[2][2] = "O"
-	board[1][2] = "X"
-	board[1][0] = "O"
-	board[0][2] = "X"
+	var s []int
+	printSlice(s)
 
-	for i := 0; i < len(board); i++ {
-		fmt.Printf("%s\n", strings.Join(board[i], " "))
-	}
+	s = append(s, 0)
+	printSlice(s)
+
+	s = append(s, 1)
+	printSlice(s)
+
+	s = append(s, 2, 3, 4)
+	printSlice(s)
+}
+
+func printSlice(slice []int) {
+	fmt.Printf("len=%d cap=%d, %v\n", len(slice), cap(slice), slice)
 }

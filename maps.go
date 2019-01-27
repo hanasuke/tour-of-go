5550c48eaf4f03b539522c911159e135cb1029bc
naosuke <info@naosuke.me>
2019-01-27 21:46:32 +0900

    :pencil: practice "Mutating Maps"

--- maps.go
+++ maps.go
@@ -2,15 +2,17 @@ package main
 
 import "fmt"
 
-type Vertex struct {
-	Lat, Long float64
-}
-
-var m = map[string]Vertex{
-	"Bell Labs": {40.68433, -74.39967},
-	"Google":    {37.42202, -122.08408},
-}
-
 func main() {
-	fmt.Println(m["Bell Labs"])
+	m := make(map[string]int)
+	m["Answer"] = 42
+	fmt.Println("The values:", m["Answer"])
+
+	m["Answer"] = 48
+	fmt.Println("The values:", m["Answer"])
+
+	delete(m, "Answer")
+	fmt.Println("The values:", m["Answer"])
+
+	v, err := m["Answer"]
+	fmt.Println("The values:", v, "Present?", err)
 }

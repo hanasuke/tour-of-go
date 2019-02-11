6b83fa8b600d4d18ba3b1c512d7b250396af5a0b
naosuke <info@naosuke.me>
2019-02-11 18:24:19 +0900

    :pencil: exercise 'fibonacci closure'

---
+++ exercise_fibonacci.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func fibonacci() func() int {
+	a := -1
+	b := 1
+	return func() int {
+		a, b = b, a+b
+		return b
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
